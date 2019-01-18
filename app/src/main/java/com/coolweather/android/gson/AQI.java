4737878d40adecb61d0613d4c1348de8a0f4d57a
fd <82283156@qq.com>
2019-01-18 16:38:53 +0800

    first commit

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2018-09-07.
+ */
+
+public class AQI {
+    public  AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
